7dd4cbcf2b56cf58cbfb1f312cbb88b29c07e41c
lichangxuan <lichangxuan@example.com>
2025-09-27 13:31:05 +0800

    修复构建错误：删除Avatar组件的导入

--- client/src/components/Art.js
+++ client/src/components/Art.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Avatar from './Avatar';
 import FilePreview from './FilePreview';
 import api from '../api';
 import { MOBILE_CONFIG } from '../config';
